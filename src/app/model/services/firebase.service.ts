620e8156a13110e095ec6b8e598107221ee45a94
yorranan <yorranan.costa@outlook.com>
2023-11-10 09:07:27 -0300

    firebase modified

--- src/app/model/services/firebase.service.ts
+++ src/app/model/services/firebase.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import Contato from '../entities/Contato';
+import { finalize } from 'rxjs/operators';
+import { AngularFireStorage} from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,10 @@ import Contato from '../entities/Contato';
 export class FirebaseService {
   private PATH : string = 'contatos';
 
-  constructor(private firestore : AngularFirestore) { }
+  constructor(
+    private firestore : AngularFirestore,
+    private storage: AngularFireStorage
+    ) {}
 
   read(){
     return this.firestore.collection(this.PATH)
@@ -29,5 +34,30 @@ export class FirebaseService {
     return this.firestore.collection(this.PATH)
     .doc(contato.id)
     .delete()
+  }
+
+  uploadImage(imagem: any, contato: Contato) {
+    const file = imagem.item(0);
+    if (file.type.split('/')[0] != 'image') {
+      console.error("Tipo não suportado!");
+      return;
+    }
+    const path = `images/${contato.id}_${file.name}`;
+    const fileRef = this.storage.ref(path);
+    let taks = this.storage.upload(path, file);
+    taks.snapshotChanges().pipe(
+      finalize(() => {
+        let uploadFileURL = fileRef.getDownloadURL();
+        uploadFileURL.subscribe(resp => {
+          contato.downloadURL = resp;
+          if (!contato.id) {
+            this.create(contato);
+          }
+          else {
+            this.update(contato, contato.id);
+          }
+        })
+      })
+    ).subscribe();
   }
 }
